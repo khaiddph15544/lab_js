d900771603275fd05ac5e9acf03d640514f04618
khaidang1123 <akhai7230@gmail.com>
2022-02-21 22:29:43 +0700

    remove a lots file

--- my-app/src/utils/reRender.js
+++
@@ -1,6 +0,0 @@
-const reRender = async (component, domElement, id = "") => {
-    document.querySelector(domElement).innerHTML = await component.print(id);
-    if (component.afterRender) component.afterRender();
-};
-
-export default reRender;
